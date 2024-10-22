f06c3fd361c5da67c8a39f019eca4d68c95665f8
Aryan Deswal <aryan.deswal@hbwsl.com>
2024-10-22 12:35:39 +0530

    Register hierarchical taxonomy 'Book Category'

--- wp-book-plugin.php
+++ wp-book-plugin.php
@@ -51,3 +51,33 @@ function wp_book_register_post_type()
 
     register_post_type('book', $args);
 }
+
+// Register hierarchical taxonomy 'Book Category'
+add_action('init', 'wp_book_register_taxonomy_category');
+function wp_book_register_taxonomy_category()
+{
+    $labels = array(
+        'name' => _x('Book Categories', 'taxonomy general name', 'wp-book'),
+        'singular_name' => _x('Book Category', 'taxonomy singular name', 'wp-book'),
+        'search_items' => __('Search Book Categories', 'wp-book'),
+        'all_items' => __('All Book Categories', 'wp-book'),
+        'parent_item' => __('Parent Book Category', 'wp-book'),
+        'parent_item_colon' => __('Parent Book Category:', 'wp-book'),
+        'edit_item' => __('Edit Book Category', 'wp-book'),
+        'update_item' => __('Update Book Category', 'wp-book'),
+        'add_new_item' => __('Add New Book Category', 'wp-book'),
+        'new_item_name' => __('New Book Category Name', 'wp-book'),
+        'menu_name' => __('Book Categories', 'wp-book'),
+    );
+
+    $args = array(
+        'hierarchical' => true,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'query_var' => true,
+        'rewrite' => array('slug' => 'book-category'),
+    );
+
+    register_taxonomy('book_category', array('book'), $args);
+}
