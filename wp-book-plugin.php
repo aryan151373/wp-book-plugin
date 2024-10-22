11c32606855611c90bdea3dfa1e839e2e0fdd2de
Aryan Deswal <aryan.deswal@hbwsl.com>
2024-10-22 12:36:33 +0530

    Register non-hierarchical taxonomy 'Book Category'

--- wp-book-plugin.php
+++ wp-book-plugin.php
@@ -81,3 +81,36 @@ function wp_book_register_taxonomy_category()
 
     register_taxonomy('book_category', array('book'), $args);
 }
+
+// Register non-hierarchical taxonomy 'Book Tag'
+add_action('init', 'wp_book_register_taxonomy_tag');
+function wp_book_register_taxonomy_tag()
+{
+    $labels = array(
+        'name' => _x('Book Tags', 'taxonomy general name', 'wp-book'),
+        'singular_name' => _x('Book Tag', 'taxonomy singular name', 'wp-book'),
+        'search_items' => __('Search Book Tags', 'wp-book'),
+        'popular_items' => __('Popular Book Tags', 'wp-book'),
+        'all_items' => __('All Book Tags', 'wp-book'),
+        'edit_item' => __('Edit Book Tag', 'wp-book'),
+        'update_item' => __('Update Book Tag', 'wp-book'),
+        'add_new_item' => __('Add New Book Tag', 'wp-book'),
+        'new_item_name' => __('New Book Tag Name', 'wp-book'),
+        'separate_items_with_commas' => __('Separate book tags with commas', 'wp-book'),
+        'add_or_remove_items' => __('Add or remove book tags', 'wp-book'),
+        'choose_from_most_used' => __('Choose from the most used book tags', 'wp-book'),
+        'menu_name' => __('Book Tags', 'wp-book'),
+    );
+
+    $args = array(
+        'hierarchical' => false,
+        'labels' => $labels,
+        'show_ui' => true,
+        'show_admin_column' => true,
+        'update_count_callback' => '_update_post_term_count',
+        'query_var' => true,
+        'rewrite' => array('slug' => 'book-tag'),
+    );
+
+    register_taxonomy('book_tag', 'book', $args);
+}
